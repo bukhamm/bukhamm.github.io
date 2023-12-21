999373b57594e181ea30806b580945edb3d8e3d0
bukhamm <69335357+bukhamm@users.noreply.github.com>
2023-12-21 14:54:18 -0300

    Correcion en Voces de Gusanos
    Se corrigen la voz de los worms

--- tesys_tap/Tap Tesis/LineEnding.cs
+++
@@ -1,7 +0,0 @@
-﻿public class LineEnding
-{
-    public string RealLineEnding { get; internal set; }
-    public string ShownLineEnding { get; internal set; }
-    public object ScintillaLineEnding { get; internal set; }
-    public string PoLineEnding { get; internal set; }
-}
